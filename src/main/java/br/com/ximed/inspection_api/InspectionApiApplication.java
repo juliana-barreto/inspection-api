b7fdb5813151be93f6deb2a66decccd6e1f0ee2e
Juliana <89608026+juliana-barreto@users.noreply.github.com>
2026-08-11 22:52:26 -0300

    feat: initialize Spring Boot application with Maven wrapper and configuration

---
+++ src/main/java/br/com/ximed/inspection_api/InspectionApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.ximed.inspection_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InspectionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InspectionApiApplication.class, args);
+	}
+
+}
