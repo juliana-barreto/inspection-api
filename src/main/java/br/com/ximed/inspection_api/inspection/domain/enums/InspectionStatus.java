b60d99f87b6abbbeb887cdbbd485ee2e1705ed9f
Juliana <89608026+juliana-barreto@users.noreply.github.com>
2026-08-12 08:43:22 -0300

    feat: expand InspectionStatus enum with additional states

--- src/main/java/br/com/ximed/inspection_api/inspection/domain/enums/InspectionStatus.java
+++ src/main/java/br/com/ximed/inspection_api/inspection/domain/enums/InspectionStatus.java
@@ -2,5 +2,9 @@ package br.com.ximed.inspection_api.inspection.domain.enums;
 
 public enum InspectionStatus {
     DRAFT,
-    FINALIZED
+    IN_PROGRESS,
+    WAITING_APPROVAL,
+    APPROVED,
+    REJECTED,
+    CANCELED
 }
