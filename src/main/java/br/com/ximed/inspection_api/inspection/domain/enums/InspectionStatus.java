35ee53a1030c2a07cf62c88b63cd2b00a54fa601
Juliana <89608026+juliana-barreto@users.noreply.github.com>
2026-08-12 18:28:33 -0300

    refactor: remove unused statuses from InspectionStatus enum

--- src/main/java/br/com/ximed/inspection_api/inspection/domain/enums/InspectionStatus.java
+++ src/main/java/br/com/ximed/inspection_api/inspection/domain/enums/InspectionStatus.java
@@ -1,10 +1,6 @@
 package br.com.ximed.inspection_api.inspection.domain.enums;
 
 public enum InspectionStatus {
-    DRAFT,
     IN_PROGRESS,
     WAITING_APPROVAL,
-    APPROVED,
-    REJECTED,
-    CANCELED
 }
