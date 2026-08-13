503a69c73342bd9dcbf9bc59b856b48503556603
Juliana <89608026+juliana-barreto@users.noreply.github.com>
2026-08-13 00:22:53 -0300

    fix: correct formatting of JPQL query in InspectionRepository

--- src/main/java/br/com/ximed/inspection_api/inspection/repository/InspectionRepository.java
+++ src/main/java/br/com/ximed/inspection_api/inspection/repository/InspectionRepository.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface InspectionRepository extends JpaRepository<Inspection, UUID> {
-        @Query("""
+    @Query("""
             SELECT DISTINCT i
             FROM Inspection i
             LEFT JOIN FETCH i.site
