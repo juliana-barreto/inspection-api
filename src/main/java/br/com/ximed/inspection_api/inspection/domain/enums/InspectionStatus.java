0e7a4902e1751128cc4ac056a052fe6006052fd4
Juliana <89608026+juliana-barreto@users.noreply.github.com>
2026-08-16 09:54:54 -0300

    feat: update InspectionStatus enum to include DRAFT, REJECTED, and APPROVED states

--- src/main/java/br/com/ximed/inspection_api/inspection/domain/enums/InspectionStatus.java
+++ src/main/java/br/com/ximed/inspection_api/inspection/domain/enums/InspectionStatus.java
@@ -1,7 +1,10 @@
 package br.com.ximed.inspection_api.inspection.domain.enums;
 
 public enum InspectionStatus {
-    IN_PROGRESS,
-    PAUSED,
-    WAITING_APPROVAL
+    DRAFT,
+    WAITING_APPROVAL,
+    REJECTED,
+    APPROVED
 }
+
+
