3286b93b5d86192b0d8c60ec8c279dcde91d7d06
Juliana <89608026+juliana-barreto@users.noreply.github.com>
2026-08-17 03:17:49 -0300

    feat: enhance RiskType enum with localized labels

--- src/main/java/br/com/ximed/inspection_api/inspection/domain/enums/RiskType.java
+++ src/main/java/br/com/ximed/inspection_api/inspection/domain/enums/RiskType.java
@@ -1,9 +1,18 @@
 package br.com.ximed.inspection_api.inspection.domain.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum RiskType {
-    PHYSICAL,
-    CHEMICAL,
-    BIOLOGICAL,
-    ERGONOMIC,
-    ACCIDENT
+    PHYSICAL("Físico"),
+    CHEMICAL("Químico"),
+    BIOLOGICAL("Biológico"),
+    ERGONOMIC("Ergonômico"),
+    ACCIDENT("Acidente");
+
+    private final String label;
+
+    RiskType(String label) {
+        this.label = label;
+    }
 }
